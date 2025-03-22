7a22477a73c922bbc433da16a1141a25a60a82bf
Agnieszka Ziarkowska-Robak <agnieszka.ziarkowska.robak@gmail.com>
2025-03-22 10:41:10 +0100

    xxx

---
+++ Sortowanie/wstawianie.py
@@ -0,0 +1,14 @@
+dane = [1, 6, 5, 8, 2,6,3,7]
+n = len(dane)
+
+for i in range (1, n):
+    aktualny= dane [i]
+    for j in range (0,i):
+        if aktualny <dane [j]:
+            dane.pop (i)
+            dane.insert (j, aktualny)
+            break
+print (dane)
+
+
+# for i in range (1, len(dane)):
