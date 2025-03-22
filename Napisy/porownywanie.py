bb0145a2338cc7f4cf1b32b4812aef2206a79770
Agnieszka Ziarkowska-Robak <agnieszka.ziarkowska.robak@gmail.com>
2025-03-22 14:59:42 +0100

    nowy1

---
+++ Napisy/porownywanie.py
@@ -0,0 +1,18 @@
+"""
+
+Ala ma kota
+Ala nie ma kota
+
+ala ma kota
+Ala nie ma kota
+
+
+Ala ma kota
+Ala ma kota
+
+Ala ma kota
+Ala ma kota i psa
+
+shortest _ len = min ( len (napis1), len (napis2))
+
+"""
