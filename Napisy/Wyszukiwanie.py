12a0c28635246991ba441f1d110d2a9800607b73
Agnieszka Ziarkowska-Robak <agnieszka.ziarkowska.robak@gmail.com>
2025-04-05 14:33:36 +0200

    wyszukiwanie naiwne2

--- Napisy/Wyszukiwanie.py
+++ Napisy/Wyszukiwanie.py
@@ -1,10 +1,17 @@
-def wyszukaj_naiwnie(sprawdzany, szukany):
+def wyszukaj_naiwnie_v2(sprawdzany, szukany):
     for i in range (len(sprawdzany)-len(szukany)+1):
-        if sprawdzany [i:i+len(szukany)]==szukany:
+       znaleziono = True
+       for j in range(len(szukany)):
+           if not sprawdzany [i+j]==szukany [j]:
+              znaleziono=True
+              break
+       if znaleziono:
            return i
     return None
 
+
+
 if __name__=='__main__':
-    sprawdzany = 'Ala ma kota'
+    sprawdzany = 'Ala ma tako'
     szukany = 'ko'
-    print(wyszukaj_naiwnie(sprawdzany,szukany))
+    print(wyszukaj_naiwnie_v2(sprawdzany,szukany))
