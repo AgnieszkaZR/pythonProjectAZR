83e2a89656978478684a6370a5a488594e2bd66f
Agnieszka Ziarkowska-Robak <agnieszka.ziarkowska.robak@gmail.com>
2025-04-06 13:48:08 +0200

    szyfr.cezara

--- szyfrowanie/szyfr.cezara.py
+++ szyfrowanie/szyfr.cezara.py
@@ -1,32 +1,25 @@
-#alfabet = ""
-#for i in range (ord('a'), ord('z')):
-#    alfabet+=(chr(i))
+alfabet = ""
+# for i in range(ord('a'), ord('z') + 1):
+#     alfabet+=(chr(i))
 import random
-alfabet = [chr(i) for i in range(ord('a'), ord('z')+1)] + list("ą,ć, ę,ł,ó, ź,ż")
-def szyfruj (napis, klucz):
+
+alfabet = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+alfabet = [chr(i) for i in range(ord('a'), ord('z') + 1)] + list("ąćęłóźż")
+random.shuffle(alfabet)
+print(alfabet)
+
+
+def szyfruj(napis, klucz):
     szyfr = [alfabet[(i + klucz) % len(alfabet)] for i in range(len(alfabet))]
-    wynik = ''
+    print(szyfr)
+    wynik = ""
     for znak in napis:
-        #if znak.islower():
-      #  if ord ('a') <= ord(znak) <=ord('z'):
-           #indeks = alfabet.index(znak)
-       #    indeks = ord(znak) - ord('a')
+        if ord('a') <= ord(znak) <= ord('z'):  # znak.islower():
+            indeks = ord(znak) - ord('a')
         if znak in alfabet:
-            #zaszyfrowany_znak = szyfr [indeks]
             indeks = alfabet.index(znak)
             zaszyfrowany_znak = szyfr[indeks]
             wynik += zaszyfrowany_znak
         else:
-           wynik+=znak
+            wynik+=znak
     return wynik
-def odszyfruj (napis,klucz):
-    pass
-if __name__== '__main__':
-    napis = "Ala ma żółć"
-    klucz=3
-
-    zaszyfrowany_napis = szyfruj(napis, klucz)
-    print(zaszyfrowany_napis)
-
-    odszyfrowany_napis= odszyfruj(zaszyfrowany_napis, klucz)
-    print(odszyfrowany_napis)
