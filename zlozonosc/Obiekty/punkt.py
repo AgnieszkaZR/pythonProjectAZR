40cf8cacfc2eaf24524ead82714fb6a26f992645
AZR <agnieszka.ziarkowska.robak@gmail.com>
2025-11-15 13:27:31 +0100

    zad..

---
+++ zlozonosc/Obiekty/punkt.py
@@ -0,0 +1,17 @@
+class Punkt:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+
+if __ name__=='__main__':
+            p = Punkt()
+
+            print(p.x, p.y)
+
+            q = Punkt()
+
+
+            p.x=5
+            p.y=10
+            print(p.x, p.y)
